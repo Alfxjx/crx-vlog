5f9af90d672d6b69c365ff6b6361ca3132c995dd
xujianxiang <xjxtju@163.com>
2021-03-01 17:48:58 +0800

    mvp

--- src/popup/root.vue
+++ src/popup/root.vue
@@ -1,24 +1,33 @@
 <template>
-  <div>
-    <el-button type="primary" @click="start()">ACTION</el-button>
-  </div>
+	<div>
+		<el-button type="primary" @click="tab()">ACTION</el-button>
+	</div>
 </template>
 <script>
-  export default {
-    data: () => ({
-    }),
-    computed: { },
-    created () { },
-    mounted () { },
-    methods: {
-      tab () {
-        chrome.tabs.create({ url: 'pages/app.html' })
-      }
-    }
-  }
+import { Vlog } from "console-vlog/dist/bundle.esm";
+export default {
+	data() {
+		return {
+			recorder: null,
+		};
+	},
+	computed: {},
+	created() {},
+	mounted() {
+		this.recorder = new Vlog({ name: "download" });
+	},
+	methods: {
+		tab() {
+			// chrome.tabs.create({ url: "pages/app.html" });
+      this.recorder.start();
+		},
+	},
+};
 </script>
 <style lang="scss">
-  div {
-    color: blue
-  }
+div {
+	color: blue;
+  width: 80rem;
+  height: 40rem;
+}
 </style>
